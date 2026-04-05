f3b5905d0bcb4f2eda0de5abe30575ea26b78a98
Flávio <f.augustosdn@gmail.com>
2026-04-04 23:33:53 -0300

    feat: implementa criptografia de senha do usuario

--- controllers/login.controller.php
+++ controllers/login.controller.php
@@ -19,14 +19,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     $usuario = $database->query(
-        query: "select * from usuarios where email = :email and senha = :senha",
+        query: "select * from usuarios where email = :email",
         class: Usuario::class,
-        params: compact('email', 'senha')
-    )
-        ->fetch();
+        params: compact('email')
+    )->fetch();
 
 
     if ($usuario) {
+        $senhaSalva = $usuario->senha;
+        $senhasIguais = password_verify($senha, $senhaSalva);
+
+        if( !$senhasIguais ){
+            flash()->push('validacoes_login', ['E-mail ou senha inválidos.']);
+            header('location: /login');
+            exit();
+        }
+
         $_SESSION['auth'] = $usuario;
         flash()->push('mensagem', "Seja bem-vindo! $usuario->nome!");
         header('location: /');
